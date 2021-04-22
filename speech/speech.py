513f8728b2cb507ba1d3d509421ab43d2e1132b8
petridisa <petridisa@csd.auth.gr>
2021-04-22 23:10:07 +0200

    Speech preprocessing

---
+++ speech/speech.py
@@ -0,0 +1,23 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+# Need to run this the first time
+# nltk.download('punkt')
+# nltk.download('wordnet')
+
+stop_words = stopwords.words('english')
+lemmatizer = WordNetLemmatizer()
+
+with open("../Datasets/txt_files/Elon_Musk_Dataset.txt", "r") as file:
+    speeches = "".join(file.readlines()[0:])
+lemmatized_speeches = []
+speeches = re.sub(r'[^\w\s]', '', speeches)  # cleaning
+words = nltk.word_tokenize(speeches)  # tokenization
+words = [w for w in words if not w in stop_words]  # stopwords removal
+for word in words:
+    if word not in stop_words:
+        lemmatized_speeches.append(lemmatizer.lemmatize(word))  # lemmatizing
+print(lemmatized_speeches)
