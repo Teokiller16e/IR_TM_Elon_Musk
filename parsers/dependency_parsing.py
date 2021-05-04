3fbb6b01398537ed7df615889a86b27e5d4a18aa
Theodoros Giannilias <32840612+Teokiller16e@users.noreply.github.com>
2021-05-04 20:43:10 +0300

    dependecy parsing tags to excel added

--- parsers/dependency_parsing.py
+++ parsers/dependency_parsing.py
@@ -1,9 +1,12 @@
+from os.path import curdir
 import numpy 
 import nltk
 from spacy.lang.en import English
 import spacy
 from spacy import displacy
 import os
+import xlsxwriter
+import pandas as pd
 
 # Dependency parsing is the process of analyzing the grammatical structure of a sentence based on the dependencies between the words in a sentence
 
@@ -26,26 +29,137 @@ def to_nltk_tree(node):
     else:
         return tok_format(node)
 
+
+
+outWorkbook = xlsxwriter.Workbook("depend.xlsx")
+outSheet = outWorkbook.add_worksheet('Sheet1')
+
 en_nlp = spacy.load('en_core_web_sm')
 # current directory IR_TM_ELON_MUSK changed--> Datasets/text_files
 cwd = os.getcwd()
 os.chdir('Datasets/txt_files')
 path = os.getcwd()
 
-f = open(path+"/Elon_Musk_Dataset.txt","r")
+f_tree = open(path+"/Elon_Musk_Dataset.txt","r")
+f = open(path+"/Elon_Musk_Dataset.txt").read()
+f_doc = en_nlp(f)
+
+# Building the xlsx file of adjectival identifier tags
+text_list = []
+dep_list = []
+head_list = []
+
+for token in f_doc:
+    text_list.append(token.text)
+
+for token in f_doc:
+    dep_list.append(token.dep_)
+
+for token in f_doc:
+    head_list.append(token.head.text)
 
 
-for line in f:
+outSheet.write('A1', 'Actual_Text')
+for row in range(2,len(text_list)+2):
+    outSheet.write(f'A{row}', text_list[row-2])
+
+
+outSheet.write('B1', 'Dependencies')
+for drow in range(2,len(dep_list)+2):
+    outSheet.write(f'B{drow}', dep_list[drow-2])
+
+
+
+outSheet.write('C1', 'Heads')
+for hrow in range(2,len(head_list)+2):
+    outSheet.write(f'C{hrow}', head_list[hrow-2])
+
+
+
+
+outWorkbook.close()
+
+
+cnt = 0
+for line in f_tree:
     #print(line)
     test = en_nlp(line)
     print(test)
     [to_nltk_tree(sent.root).pretty_print() for sent in test.sents]
-
-
-#    displacy.render(nlp(text),jupyter=True)
-
-    #command="Submit debug logs to project lead today at 9:00 AM"
-    #print("hello world")
-    #en_doc=en_nlp(u''+command)
-
     
+
+"""
+d
+
+
+
+en_nlp = spacy.load('en_core_web_sm')
+#doc = "Apple is looking at buying U.K. startup for $1 billion"
+
+cwd = os.getcwd()
+os.chdir('Datasets/txt_files')
+path = os.getcwd()
+
+f = open(path+"/Elon_Musk_Dataset.txt").read()
+
+#f_doc = nlp(f)
+
+
+for line in f:
+    test = en_nlp(line)
+    print(test)
+    [to_nltk_tree(sent.root).pretty_print() for sent in test.sents]
+
+#print(displacy.render(f_doc, jupyter=True))
+#print(displacy.render(f_doc, style='dep', jupyter = True, options = {'distance': 120}))
+
+# edw ksekinaei to list thing
+text_list = []
+dep_list = []
+head_list = []
+
+for token in f_doc:
+    text_list.append(token.text)
+
+for token in f_doc:
+    dep_list.append(token.dep_)
+
+for token in f_doc:
+    head_list.append(token.head.text)
+
+
+outSheet.write('A1', 'Actual_Text')
+for row in range(2,len(text_list)+2):
+    outSheet.write(f'A{row}', text_list[row-2])
+
+
+outSheet.write('B1', 'Dependencies')
+for drow in range(2,len(dep_list)+2):
+    outSheet.write(f'B{drow}', dep_list[drow-2])
+
+
+
+outSheet.write('C1', 'Heads')
+for hrow in range(2,len(head_list)+2):
+    outSheet.write(f'C{hrow}', head_list[hrow-2])
+
+
+
+
+outWorkbook.close()
+
+"""
+"""
+for token in f_doc:
+    #print(token.text,'==>',  token.dep_,'==>',token.head.text,'\n')
+    strA = "A"+str(i)
+    strB = "B"+str(j)
+    strC = "C"+str(k)
+    outSheet.write(strA,i,token.text)
+    outSheet.write(strB,j,token.dep_)
+    outSheet.write(strC,k,token.head.text)
+    i= i+1
+    j= j+1
+    k= k+1
+
+"""
